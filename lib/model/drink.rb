f9d3d4693bb120f16b96bf4ce7cfabe3067acc13
Amy Nguyen <amy.nguyen730@gmail.com>
2019-10-24 11:11:03 -0600

    added images

--- lib/model/drink.rb
+++ lib/model/drink.rb
@@ -26,63 +26,79 @@ class Drink < ActiveRecord::Base
   end
 
   def self.long_island_tea
+    system("imgcat ./lib/pics/long_island_tea.jpg")
     puts ingredients_and_quantity("Long Island Tea")
     puts ""
     puts instructions("Long Island Tea")
   end
 
   def self.negroni
+    system("imgcat ./lib/pics/negroni.jpg")
     puts ingredients_and_quantity("Negroni")
     puts ""
     puts instructions("Negroni")
   end
   
   def self.dry_martini
+    system("imgcat ./lib/pics/dry-martini.jpg")
     puts ingredients_and_quantity("Dry Martini")
     puts ""
     puts instructions("Dry Martini")
   end
 
   def self.alabama_slammer
+    system("imgcat ./lib/pics/southern_comfort.jpg")
     puts ingredients_and_quantity("Alabama Slammer")
     puts ""
     puts instructions("Alabama Slammer")
   end
 
   def self.margarita
+    system("imgcat ./lib/pics/margarita.jpg")
     puts ingredients_and_quantity("Margarita")
     puts ""
     puts instructions("Margarita")
   end
 
   def self.manhattan
+    system("imgcat ./lib/pics/Manhattan.jpg")
     puts ingredients_and_quantity("Manhattan")
     puts ""
     puts instructions("Manhattan")
   end
 
   def self.algonquin
+    system("imgcat ./lib/pics/Algonquin.jpg")
     puts ingredients_and_quantity("Algonquin")
     puts ""
     puts instructions("Algonquin")
   end
 
   def self.allegheny
+    system("imgcat ./lib/pics/Allegheny.jpg")
     puts ingredients_and_quantity("Allegheny")
     puts ""
     puts instructions("Allegheny")
   end
 
   def self.daiquiri
+    system("imgcat ./lib/pics/daiquiri.jpg")
     puts ingredients_and_quantity("Daiquiri")
     puts ""
     puts instructions("Daiquiri")
   end
 
   def self.old_fashioned
+    system("imgcat ./lib/pics/old-fashioned.jpg")
     puts ingredients_and_quantity("Old Fashioned")
     puts ""
     puts instructions("Old Fashioned")
+  end
+
+  def self.allies_cocktail
+    puts ingredients_and_quantity("Allies Cocktail")
+    puts ""
+    puts instructions("Allies Cocktail")
   end
 
   def self.vodka
@@ -91,8 +107,6 @@ class Drink < ActiveRecord::Base
 
     system "clear"
 
-    # system("imgcat ../pics/Vodka.jpeg")
-
     response = @@prompt.select(choice, selection, active_color: :bright_blue)
     case response
       when "Long Island Tea"
@@ -100,11 +114,13 @@ class Drink < ActiveRecord::Base
         long_island_tea
       when "Moscow Mule"
         system "clear"
+        system("imgcat ./lib/pics/moscow-mule.jpg")
         puts ingredients_and_quantity("Moscow Mule")
         puts ""
         puts instructions("Moscow Mule")
       when "Alfie Cocktail"
         system "clear"
+        system("imgcat ./lib/pics/alfie_cocktail.jpg")
         puts ingredients_and_quantity("Alfie Cocktail")
         puts ""
         puts instructions("Alfie Cocktail")
@@ -134,19 +150,19 @@ class Drink < ActiveRecord::Base
         alabama_slammer
       when "Alaska Cocktail"
         system "clear"
+        system("imgcat ./lib/pics/alaska_cocktail.jpg")
         puts ingredients_and_quantity("Alaska Cocktail")
         puts ""
         puts instructions("Alaska Cocktail")
       when "Alexander"
         system "clear"
+        system("imgcat ./lib/pics/alexander.jpg")
         puts ingredients_and_quantity("Alexander")
         puts ""
         puts instructions("Alexander")
       when "Allies Cocktail"
         system "clear"
-        puts ingredients_and_quantity("Allies Cocktail")
-        puts ""
-        puts instructions("Allies Cocktail")
+        allies_cocktail
     end
     puts ""
     Application.exit_option
@@ -194,9 +210,7 @@ class Drink < ActiveRecord::Base
         allegheny
       when "Allies Cocktail"
         system "clear"
-        puts ingredients_and_quantity("Allies Cocktail")
-        puts ""
-        puts instructions("Allies Cocktail")
+        allies_cocktail
     end
     puts ""
     Application.exit_option
@@ -211,6 +225,7 @@ class Drink < ActiveRecord::Base
     case response
       when "Mojito"
         system "clear"
+        system("imgcat ./lib/pics/Mojito.jpg")
         puts ingredients_and_quantity("Mojito")
         puts ""
         puts instructions("Mojito")
@@ -241,6 +256,7 @@ class Drink < ActiveRecord::Base
         old_fashioned
       when "Whiskey Sour"
         system "clear"
+        system("imgcat ./lib/pics/whiskey-sour.jpg")
         puts ingredients_and_quantity("Whiskey Sour")
         puts ""
         puts instructions("Whiskey Sour")
@@ -249,11 +265,13 @@ class Drink < ActiveRecord::Base
         manhattan
       when "After Dinner Cocktail"
         system "clear"
+        system("imgcat ./lib/pics/after_dinner_cocktail.jpg")
         puts ingredients_and_quantity("After Dinner Cocktail")
         puts ""
         puts instructions("After Dinner Cocktail")
       when "After Supper Cocktail"
         system "clear"
+        system("imgcat ./lib/pics/after_supper_cocktail.jpg")
         puts ingredients_and_quantity("After Supper Cocktail")
         puts ""
         puts instructions("After Supper Cocktail")
@@ -262,6 +280,7 @@ class Drink < ActiveRecord::Base
         alabama_slammer
       when "Brandy Alexander"
         system "clear"
+        system("imgcat ./lib/pics/alexander.jpg")
         puts ingredients_and_quantity("Brandy Alexander")
         puts ""
         puts instructions("Brandy Alexander")
